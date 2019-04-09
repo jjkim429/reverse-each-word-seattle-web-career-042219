c179429f3d46445b55454278f9790d54bfd974ac
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 18:53:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split
   newarray = []
-  array.each do |x|
+  array.collect do |x|
     newarray.push (x.reverse)
   end
   return newarray.join(" ")
